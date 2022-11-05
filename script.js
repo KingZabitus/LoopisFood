ef31487e3ec564f1a1f378f230effc2069c2b9c7
Marcos Paulo <103538721+KingZabitus@users.noreply.github.com>
2022-11-05 12:39:31 -0300

    Atualização de Script
    Finalizando as funções do código.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const desconto = (p) => p - (p*(20/100));
+
 let nomeProduto = document.querySelector("#nome-do-produto");
-let preco = document.querySelector("#preço");
+let precoInput = document.querySelector("#preço");
 let promo = document.querySelector("#promocao");
 const saveButt = document.querySelector("#salvar");
 const produtos = document.querySelector("#produtos");
@@ -7,16 +9,27 @@ const produtos = document.querySelector("#produtos");
 saveButt.addEventListener('click', () => {
     let novaDiv = document.createElement('div');
     let nome = document.createElement('p');
-    let preço = document.createElement('p');
+    nome.classList.add("nome");
+    let ValorPreço = document.createElement('p');
+    ValorPreço.classList.add("valor-preço");
     let button = document.createElement('button');
+    button.classList.add("botao-de-remover");
+    if(document.getElementById('sim').checked){
+        ValorPreço.textContent = desconto(precoInput.value);
+        produtos.appendChild(novaDiv);
+        novaDiv.classList.add("preco-com-desconto");
+    }else{
+        ValorPreço.textContent = precoInput.value;
+        produtos.prepend(novaDiv);
+        novaDiv.classList.add("preco-sem-desconto");
+    }
     nome.textContent = nomeProduto.value;
-    preço.textContent = preco.value;
     button.textContent = "Remover";
-    produtos.appendChild(novaDiv);
     novaDiv.appendChild(nome);
-    novaDiv.appendChild(preço);
+    novaDiv.appendChild(ValorPreço);
     novaDiv.appendChild(button);
     button.addEventListener('click', () => {
         produtos.removeChild(novaDiv);
-    })
+    });
+
 });
