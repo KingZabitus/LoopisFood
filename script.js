4aac22eb108ef9866a4283d76eb3830b1ea4059b
Marcos Paulo <103538721+KingZabitus@users.noreply.github.com>
2022-11-09 10:37:09 -0300

    Atualizando o script
    Adicionamos o salvamento dos produtos no localStorage

--- script.js
+++ script.js
@@ -2,34 +2,44 @@ const desconto = (p) => p - (p*(20/100));
 
 let nomeProduto = document.querySelector("#nome-do-produto");
 let precoInput = document.querySelector("#preço");
-let promo = document.querySelector("#promocao");
 const saveButt = document.querySelector("#salvar");
 const produtos = document.querySelector("#produtos");
+let binary;
+let array = JSON.parse(localStorage.getItem('prod')) || []
+
 
 saveButt.addEventListener('click', () => {
     let novaDiv = document.createElement('div');
     let nome = document.createElement('p');
     nome.classList.add("nome");
+    nome.textContent = nomeProduto.value;
     let ValorPreço = document.createElement('p');
     ValorPreço.classList.add("valor-preço");
     let button = document.createElement('button');
     button.classList.add("botao-de-remover");
+    button.textContent = "Remover";
+    button.addEventListener('click', () => {
+        produtos.removeChild(novaDiv);
+    })
     if(document.getElementById('sim').checked){
         ValorPreço.textContent = desconto(precoInput.value);
         produtos.appendChild(novaDiv);
         novaDiv.classList.add("preco-com-desconto");
-    }else{
+        binary = 1;
+    } else{
         ValorPreço.textContent = precoInput.value;
         produtos.prepend(novaDiv);
         novaDiv.classList.add("preco-sem-desconto");
+        binary = 0;
     }
-    nome.textContent = nomeProduto.value;
-    button.textContent = "Remover";
     novaDiv.appendChild(nome);
     novaDiv.appendChild(ValorPreço);
     novaDiv.appendChild(button);
-    button.addEventListener('click', () => {
-        produtos.removeChild(novaDiv);
-    });
-
+    let produto = {
+        desconto: binary,
+    }
+    produto.nome = nomeProduto.value;
+    produto.preço = precoInput.value;
+    array.push(produto);
+    localStorage.setItem("prod", JSON.stringify(array));
 });
